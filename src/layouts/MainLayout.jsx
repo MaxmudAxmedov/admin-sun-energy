99b8edaa25ee24ed2a7f77e55f78550fd58ec8ef
sadriddinovtohir <toxir4626@gmail.com>
2025-09-15 18:16:48 +0500

    layout change

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -10,6 +10,7 @@ export default function MainLayout() {
             <div>
                 {/* HEADER dynamic */}
                 <Outlet />
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, facere? Aut non eligendi quae sunt laudantium numquam vel. Earum est autem nemo ut accusamus tempora, repellat pariatur consequuntur in sunt.
             </div>
         </div>
     );
