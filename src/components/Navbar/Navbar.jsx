2ed0369b4d106f931841bc34899ce8efdcc72574
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2025-09-23 17:59:10 +0500

    ...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,58 +2,55 @@ import React from "react";
 import { data } from "./data";
 import { NavLink, useLocation } from "react-router-dom";
 import logo_img from "../../assets/img/logo_img.png";
-import { useTranslation } from "react-i18next";
 export default function Navbar() {
-  const location = useLocation();
-  const { t } = useTranslation();
-  return (
-    <div className="w-[270px] h-[100vh] overflow-hidden ">
-      <NavLink
-        to={"/"}
-        className={"w-[100%] flex no-underline items-center  gap-3"}
-      >
-        <img
-          className="w-[60px] ml-2 mt-[10px] rounded-md"
-          src={logo_img}
-          alt="img"
-        />
-        <h3 className="flex items-center text-aside gap-1 mt-[10px] no-underline rounded-xl w-[200px]   transition-all duration-500 ease-in-out">
-          SUN ENERGIY
-        </h3>
-      </NavLink>
-      <ul className="p-5 w-[340px]  h-[100vh] bg-[var(--background)] list-none">
-        {data.map((item, index) => {
-          const Icons = item.icon;
-          return (
-            <li className="mb-1" key={index}>
-              <NavLink
-                to={item.path}
-                className={({ isActive }) =>
-                  `flex items-center gap-1  no-underline rounded-xl w-[200px]   transition-all duration-500 ease-in-out
+    const location = useLocation();
+    return (
+        <div className="w-[270px] h-[100vh] overflow-hidden ">
+            <NavLink
+                to={"/"}
+                className={"w-[100%] flex no-underline items-center  gap-3"}
+            >
+                <img
+                    className="w-[60px] ml-2 mt-[10px] rounded-md"
+                    src={logo_img}
+                    alt="img"
+                />
+                <h3 className="flex items-center text-aside gap-1 mt-[10px] no-underline rounded-xl w-[200px]   transition-all duration-500 ease-in-out">
+                    SUN ENERGIY
+                </h3>
+            </NavLink>
+            <ul className="p-5 w-[340px]  h-[100vh] bg-[var(--background)] list-none">
+                {data.map((item, index) => {
+                    const Icons = item.icon;
+                    return (
+                        <li className="mb-1" key={index}>
+                            <NavLink
+                                to={item.path}
+                                className={({ isActive }) =>
+                                    `flex items-center gap-1  no-underline rounded-xl w-[200px]   transition-all duration-500 ease-in-out
                 ${
-                  isActive
-                    ? "text-[var(--active-text)] shadow-md"
-                    : "text-[var(--black)]"
+                    isActive
+                        ? "text-[var(--active-text)] shadow-md"
+                        : "text-[var(--black)]"
                 }`
-                }
-              >
-                {" "}
-                <div
-                  className={`w-10 h-10  p-2  rounded-md  pt-2 m-2 transition-all duration-500 ease-in-out
+                                }
+                            >
+                                <div
+                                    className={`w-10 h-10  p-2  rounded-md  pt-2 m-2 transition-all duration-500 ease-in-out
                 ${
-                  location.pathname === item.path
-                    ? "bg-[var(--icons-colors)] text-[var(--background)] shadow-md"
-                    : "bg-[var( --icons-colors)] text-[var(--black)]"
+                    location.pathname === item.path
+                        ? "bg-[var(--icons-colors)] text-[var(--background)] shadow-md"
+                        : "bg-[var( --icons-colors)] text-[var(--black)]"
                 }`}
-                >
-                  {Icons && <Icons />}
-                </div>{" "}
-                {t(item.title.toLowerCase())}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+                                >
+                                    {Icons && <Icons />}
+                                </div>{" "}
+                                {item.title}
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
