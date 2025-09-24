07dafc8673eb0904ced3d3b2d7cd4be2ca1c32db
omonboyweb <xojamurodov404@gmail.com>
2025-09-24 17:25:04 +0500

    repairing i18n

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./utils/i18n.js";
 
 createRoot(document.getElementById("root")).render(
-    <I18nextProvider i18n={i18n}>
+  <I18nextProvider i18n={i18n}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-    </I18nextProvider>
-
+  </I18nextProvider>
 );
